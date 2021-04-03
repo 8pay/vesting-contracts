2ec83fce22626ac9ae7e5e02487898ed54103988
AleG94 <alegen.dev@gmail.com>
2021-04-03 17:18:51 +0200

    chore: update mainnet owner

--- data/owner.js
+++ data/owner.js
@@ -6,6 +6,6 @@ module.exports = {
     address: '0x5B848132d3a0111d4daB7060b6051961013C71c7',
   },
   mainnet: {
-    address: '0x5B848132d3a0111d4daB7060b6051961013C71c7',
+    address: '0xD8BEC82Dd5C0FE69f012de6660C4a44d63b786BD',
   },
 };
